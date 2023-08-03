ca293bfd9a7537c260b2cc0da7af9b5bde0de878
root <odungideemediong@gmail.com>
2023-08-03 22:51:26 +0000

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
-"""A module for encrypting passwords.
+"""
+Password Hashing and validation.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    Hash password string using bcrypt
+    params:
+        - password (str): password string to be hashed
+    return:
+        - (bytes): hashed password
+    """
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed_password
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks is a hashed password was formed from the given password.
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)￼Enter
+    validate password.
+    params:
+        - hashed_password (bytes): bcrypt hashed password
+        - password (str): plain password string
+    return:
+        - (bool): True if password match else False
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
